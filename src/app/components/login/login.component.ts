9dfee6fecf8ac61b653011342d4bb0e1524ba652
samke404 <you@example.com>
2023-02-16 10:30:36 +0200

    Authentication & Authorization

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
-
+import { FormBuilder, FormControl } from '@angular/forms';
+import { FormGroup } from '@angular/forms';
+import { Validators } from '@angular/forms';
+import ValidateForm from 'src/app/helpers/validatorform';
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
@@ -10,15 +13,28 @@ export class LoginComponent implements OnInit {
   type: string = "password";
   isText: boolean = false;
   eyeIcon: string = "fa-eye-slash"
-
-  constructor() { }
+  loginForm!: FormGroup;
+  constructor(private fb: FormBuilder) { }
 
   ngOnInit(): void {
-  }
+    this.loginForm = this.fb.group({
+    username: ['',Validators.required],
+    password: ['',Validators.required]
+  })
+}
   hideShowPass(){
     this.isText = !this.isText;
     this.isText ? this.eyeIcon = "fa-eye" : this.eyeIcon = "fa-eye-slash";
     this.isText ? this.type = "text" : this.type = "password";
+  }
+   onSubmit(){
+    if(this.loginForm.valid){
+      console.log(this.loginForm.value);
+    }else{
+      //throw error
+      ValidateForm.validateAllFormFields(this.loginForm);
+      alert("Your form is invalid")
+    }
   }
 
 }
--- src/app/components/signup/signup.component.ts
+++ src/app/components/signup/signup.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
-
+import { FormBuilder, FormControl } from '@angular/forms';
+import { FormGroup } from '@angular/forms';
+import { Validators } from '@angular/forms';
+import ValidateForm from 'src/app/helpers/validatorform';
 @Component({
   selector: 'app-login',
   templateUrl: './signup.component.html',
@@ -10,15 +13,33 @@ export class SignupComponent implements OnInit {
   type: string = "password";
   isText: boolean = false;
   eyeIcon: string = "fa-eye-slash"
+  signupForm!: FormGroup;
 
-  constructor() { }
+  constructor(private fb: FormBuilder) { }
 
   ngOnInit(): void {
+    this.signupForm = this.fb.group({
+      name:['',Validators.required],
+      surname:['',Validators.required],
+      email:['',Validators.required],
+      username:['',Validators.required],
+      password:['',Validators.required],
+    })
   }
   hideShowPass(){
     this.isText = !this.isText;
     this.isText ? this.eyeIcon = "fa-eye" : this.eyeIcon = "fa-eye-slash";
     this.isText ? this.type = "text" : this.type = "password";
+  }
+  onSubmit(){
+    if(this.signupForm.valid){
+      //perfom sign up logic
+      console.log(this.signupForm.value);
+    }else{
+      //throw error
+      ValidateForm.validateAllFormFields(this.signupForm);
+      alert("Your form is invalid")
+    }
   }
 
 }
